8c1fbee68697a0811e0adb70d147de650ef514a0
iamdeepakrana1 <dr24abs@herts.ac.uk>
2025-12-11 00:13:49 +0000

    Filtering data set

--- surveyCode.R
+++ surveyCode.R
@@ -3,3 +3,6 @@ library(tidyverse)
 mh <- read_csv("survey.csv")
 
 head(mh, 5)
+
+#2 Filtering required columns and removing rows with null values
+mh <- mh %>% select(Timestamp, Age, treatment) %>% drop_na()
