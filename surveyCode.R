6f95eff8b8924b19fceebd9416de10550ad73940
iamdeepakrana1 <dr24abs@herts.ac.uk>
2025-12-11 00:39:52 +0000

    Validating treatment column

--- surveyCode.R
+++ surveyCode.R
@@ -12,3 +12,10 @@ mh <- mh %>% filter(substr(Timestamp, 1, 4) == "2014")
 mh <- mh %>% filter(Age >= 18, Age <= 70)
 
 
+#4 validating the treatment column to 'Yes' or 'No'
+mh <- mh %>%
+  mutate(treatment = str_trim(treatment),
+         treatment = case_when(
+           str_to_lower(treatment) %in% c("yes","y","yes ") ~ "Yes",
+           str_to_lower(treatment) %in% c("no","n","no ")  ~ "No",
+           TRUE ~ as.character(treatment)))
