e6c3f3f5f0b1f5f75c0b537a635584881e88c5ca
iamdeepakrana1 <dr24abs@herts.ac.uk>
2025-12-11 00:37:22 +0000

    Filtering data within 2014

--- surveyCode.R
+++ surveyCode.R
@@ -6,3 +6,9 @@ head(mh, 5)
 
 #2 Filtering required columns and removing rows with null values
 mh <- mh %>% select(Timestamp, Age, treatment) %>% drop_na()
+
+#3 filtering data within 2014 and age between 18 and 70
+mh <- mh %>% filter(substr(Timestamp, 1, 4) == "2014")
+mh <- mh %>% filter(Age >= 18, Age <= 70)
+
+
